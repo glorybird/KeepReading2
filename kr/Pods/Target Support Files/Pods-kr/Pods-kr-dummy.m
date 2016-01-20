e4e43be363b0ba1ec4bc7bebdcc1f1443af69c4c
fanfei <hzfanfei@corp.netease.com>
2016-01-20 22:00:08 +0800

    pod install

---
+++ kr/Pods/Target Support Files/Pods-kr/Pods-kr-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kr : NSObject
+@end
+@implementation PodsDummy_Pods_kr
+@end
---
+++ kr/Pods/Target Support Files/Pods-kr/Pods-kr-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_krVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_krVersionString[];
+
