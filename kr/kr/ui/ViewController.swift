b62ecd781ba5e6e6bdb8c36d00457de79f654994
fanfei <hzfanfei@corp.netease.com>
2016-01-20 20:31:40 +0800

    初始化工程

---
+++ kr/kr/ui/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kr
+//
+//  Created by FanFamily on 16/1/20.
+//  Copyright © 2016年 glorybird. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
